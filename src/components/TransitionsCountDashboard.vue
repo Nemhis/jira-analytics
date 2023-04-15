e1e960402ecca7d8c000363d6e3ad612f7fb0f6a
p.kurapov <p.nemphis@gmail.com>
2023-04-15 10:07:24 +0300

    Install chart js

--- src/components/TransitionsCountDashboard.vue
+++ src/components/TransitionsCountDashboard.vue
@@ -1,7 +1,18 @@
 <template>
-  <div v-for="counter in total" :key="`${counter.from}-${counter.to}`">
-    {{ getStatusName(counter.from) }} -> {{ getStatusName(counter.to) }} <br />
-    count: {{ counter.count }} <br />
+  <div style="padding: 20px 50px">
+    <bar-chart
+      :chart-data="total"
+      :options="{
+        backgroundColor: $vuetify.theme.current.colors.primary,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Количество переводов задач по статусам',
+            color: $vuetify.theme.current.colors.secondary,
+          },
+        },
+      }"
+    />
   </div>
 </template>
 
@@ -13,6 +24,10 @@ import { TransitionCount } from '@/interfaces/TransitionCount';
 import { serviceProvider } from '@/plugins/services';
 import { useJiraStore } from '@/store/jira';
 import { computed, defineProps, onMounted, ref, watch } from 'vue';
+import { Chart, BarController, CategoryScale, LinearScale, BarElement, Title } from 'chart.js';
+import { BarChart } from 'vue-chart-3';
+
+Chart.register(BarController, CategoryScale, LinearScale, BarElement, Title);
 
 const props = defineProps<{
   resourceId: string;
@@ -21,13 +36,32 @@ const props = defineProps<{
 }>();
 const jiraStore = useJiraStore();
 const counts = ref<TransitionCount[]>([]);
-const total = computed(() => serviceProvider.changelogService.aggregateTransitionsCount(counts.value));
+const total = computed(() => {
+  const aggregated = serviceProvider.changelogService.aggregateTransitionsCount(counts.value);
+  aggregated.sort((a: TransitionCount, b: TransitionCount): number => a.from - b.from);
+  const getLabel = (counter: TransitionCount) => `${getStatusName(counter.from)} -> ${getStatusName(counter.to)}`;
+
+  return {
+    labels: aggregated.map(getLabel),
+    datasets: [
+      {
+        data: aggregated.map(({ count }: TransitionCount) => count),
+      },
+    ],
+  };
+});
 
 onMounted(() => {
   jiraStore.loadWorkflowStatuses(props.resourceId).then();
 });
 
 const loadChangelogs = () => {
+  counts.value = [];
+
+  if (props.issues.length === 0) {
+    return;
+  }
+
   props.issues.forEach((issue: Issue) => {
     jiraStore.loadChangelog(props.resourceId, issue.key).then((list: PaginatedList<Changelog>) => {
       const loadedCounts: TransitionCount[] = serviceProvider.changelogService.calcTransitionsCount(list.values);
@@ -45,8 +79,10 @@ const getStatusName = (id: number): string => {
 
 watch(
   () => props.loading,
-  (): void => {
-    loadChangelogs();
+  (loading: boolean): void => {
+    if (loading === false) {
+      loadChangelogs();
+    }
   },
   { immediate: true }
 );
